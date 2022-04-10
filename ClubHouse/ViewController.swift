f6b9202e3e20741b503fe9cea5919dec7830218d
OlegKotkov <tigrastov@gmail.com>
2022-04-10 19:57:33 +0200

    usage of snapKit

--- ClubHouse/ViewController.swift
+++ ClubHouse/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  ClubHouse
-//
-//  Created by mac on 25.02.2022.
-//
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialize()
+        
+        
     }
 
+    private func initialize() {
+        view.backgroundColor = UIColor (red: 236/255, green: 226/255, blue: 241/255, alpha: 1)
+        let label = UILabel()
+        label.text = "🍏Welcome"
+        label.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(label)
+        label.snp.makeConstraints { maker in
+            maker.left.equalToSuperview().inset(50)
+            maker.top.equalToSuperview().inset(150)
+        }
 
+        let textLable = UILabel()
+        textLable.text = "Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text Lable Text"
+        view.addSubview(textLable)
+        textLable.numberOfLines = 0
+        textLable.snp.makeConstraints { maker in
+            maker.left.right.equalToSuperview().inset(50)
+            maker.top.equalTo(label).inset(50)
+        }
+        
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 84/255, green: 118/255, blue: 171/255, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 20
+        button.setTitle("Get your username ->", for: .normal)
+        view.addSubview(button)
+        button.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.width.equalTo(250)
+            maker.height.equalTo(40)
+            maker.bottom.equalToSuperview().inset(30)
+            
+        }
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc private func buttonTapped() {
+       print("We want to get a username")
+    }
+    
 }
 
